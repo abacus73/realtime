a1afbd64580f9bddfc3ae5a205d47aeb14062441
abacus73 <yuxuan26@gmail.com>
2014-08-19 12:28:51 +0800

    discovery click data inspector

--- handler.py
+++ handler.py
@@ -2,11 +2,42 @@
 from helper import *
 
 class Handler():
-    def __init__(self, url):
+    def __init__(self, url, date_key):
         self.url = url
+        self.date_key = date_key
 
-    def discovery_click_generator():
+    def discovery_click_inspector(discovery_click_data, discovery_level_dict={}):
         url = self.url
+        date_key = self.date_key
+
+        if url.startswith('/api/1/discovery/item'):
+            if date_key not in discovery_click_data:
+                discovery_click_data[date_key] = {
+                    'daily_all_clicks': 0,
+                    'daily_essence_clicks': 0
+                }
+
+            daily_data = discovery_click_data[date_key]
+            # print url
+            result = re.search(discovery_oid_regex, url)
+            if result:
+                oid = result.group(1)
+
+                if oid not in discovery_level_dict:
+                    discovery = mongo_db.discovery.find_one({'_id':ObjectId(oid)}, {'level':1})
+                    if discovery:
+                        level = discovery.get('level', 1)
+                    else:
+                        level = 0
+
+                    discovery_level_dict[oid] = level
+                else:
+                    level = discovery_level_dict[oid]
+
+                daily_data['daily_all_clicks'] += 1
+
+                if level >= 3:
+                    daily_data['daily_essence_clicks'] += 1
 
 
     def discovery_list():
--- main.py
+++ main.py
@@ -34,37 +34,11 @@ for msg in consumer:
 
         url = result.group(2)
 
+        url_handler = Handler(url, date_key)
         # print log_time, url
 
         # 笔记点击
-        if url.startswith('/api/1/discovery/item'):
-            if date_key not in discovery_click_data:
-                discovery_click_data[date_key] = {
-                    'daily_all_clicks': 0,
-                    'daily_essence_clicks': 0
-                }
-
-            daily_data = discovery_click_data[date_key]
-            # print url
-            result = re.search(discovery_oid_regex, url)
-            if result:
-                oid = result.group(1)
-
-                if oid not in discovery_dict:
-                    discovery = mongo_db.discovery.find_one({'_id':ObjectId(oid)}, {'level':1})
-                    if discovery:
-                        level = discovery.get('level', 1)
-                    else:
-                        level = 0
-
-                    discovery_dict[oid] = level
-                else:
-                    level = discovery_dict[oid]
-
-                daily_data['daily_all_clicks'] += 1
-
-                if level >= 3:
-                    daily_data['daily_essence_clicks'] += 1
+        url_handler.discovery_click_inspector(discovery_click_data, discovery_level_dict=discovery_dict)
 
         # # 类别列表点击
         # if url.startswith('/api/1/discovery/list') and url.find('start=5') < 0:
