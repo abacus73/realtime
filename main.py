488f1208c6cbfcbff5267aa863013cfe73c211ff
abacus73 <yuxuan26@gmail.com>
2014-08-20 18:11:14 +0800

    test discovery clicks

--- main.py
+++ main.py
@@ -40,14 +40,14 @@ for msg in consumer:
         # 笔记点击
         url_handler.discovery_click_inspector(discovery_click_data, discovery_level_dict=discovery_dict)
 
-        # 类别列表点击
-        url_handler.discovery_list_inspector(list_oid_click_data)
+        # # 类别列表点击
+        # url_handler.discovery_list_inspector(list_oid_click_data)
 
-        # 专题页点击
-        url_handler.event_page_click_inspector(event_page_click_data)
+        # # 专题页点击
+        # url_handler.event_page_click_inspector(event_page_click_data)
 
-        # 专题页微信分享
-        url_handler.event_page_weixin_share_inspector(event_page_fpid_data)
+        # # 专题页微信分享
+        # url_handler.event_page_weixin_share_inspector(event_page_fpid_data)
 
 
         # 判断是否达到分页值，进行提交
@@ -58,17 +58,17 @@ for msg in consumer:
             url_handler.discovery_click_handler(discovery_click_data)
             discovery_click_data = {}
 
-            # 提交类别列表点击数
-            url_handler.discovery_list_handler(list_oid_click_data)
-            list_oid_click_data = {}
+            # # 提交类别列表点击数
+            # url_handler.discovery_list_handler(list_oid_click_data)
+            # list_oid_click_data = {}
 
-            # 提交专题页点击数
-            url_handler.event_page_click_handler(event_page_click_data)
-            event_page_click_data = {}
+            # # 提交专题页点击数
+            # url_handler.event_page_click_handler(event_page_click_data)
+            # event_page_click_data = {}
 
-            # 提交专题页微信分享数
-            url_handler.event_page_weixin_share_handler(event_page_fpid_data)
-            event_page_fpid_data = {}
+            # # 提交专题页微信分享数
+            # url_handler.event_page_weixin_share_handler(event_page_fpid_data)
+            # event_page_fpid_data = {}
 
 
 kafka.close()
